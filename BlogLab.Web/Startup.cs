e5f262ef450e519c514cc9f80962b9043742a1c1
elprs <parisi.code@gmail.com>
2021-09-26 21:50:55 +0300

    Configuring StartUp

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,12 +1,21 @@
+using BlogLab.Identity;
+using BlogLab.Repository;
+using BlogLab.Services;
+using BlogLabModels.Account;
 using BlogLabModels.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogLab.Web
@@ -16,16 +25,60 @@ namespace BlogLab.Web
         public IConfiguration Configuration { get; }
         public Startup(IConfiguration config)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = config;
         }
-      
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
-            services.AddRazorPages();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddScoped<IBlogrepository, BlogRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+
+            }).AddUserStore<UserStore>()
+            .AddDefaultTokenProviders()
+            .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+            services.AddControllers();
+            services.AddCors();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(
+                options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration
+                        ["Jwt:Key"])),
+                        ClockSkew = TimeSpan.Zero
+
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
